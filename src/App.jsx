8e2d78817d88db11158ab41134a57766dcd48269
Junior Guimarães <77200196+nioritos@users.noreply.github.com>
2022-07-25 19:06:34 -0300

    edited the import <skills/>

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './css/App.css'
 import Head   from './componets/header/Header';
 import Home   from './componets/home/Home';
 import About   from './componets/about/About';
-import Skills  from './componets/skills/Skills';
+import Skills  from './componets/skills/skills';
 import Footer  from './componets/footer/Footer';
 import Projects from './componets/project/projects';
 import Services from './componets/services/Services';
